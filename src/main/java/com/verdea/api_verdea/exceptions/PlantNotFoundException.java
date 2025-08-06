20e1ae775745a96a50330272514bcccdd52d67de
LucasVitorVD <lucaskirito85@gmail.com>
2025-08-06 16:26:26 -0300

    feat: deleting and update plants endpoints

---
+++ src/main/java/com/verdea/api_verdea/exceptions/PlantNotFoundException.java
@@ -0,0 +1,7 @@
+package com.verdea.api_verdea.exceptions;
+
+public class PlantNotFoundException extends RuntimeException {
+    public PlantNotFoundException(String message) {
+        super(message);
+    }
+}
